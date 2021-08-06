a2333b441d6aa1dc31d1c8f257589eb32cbe5077
Anastasiia <fdcz3322@gmail.com>
2021-08-06 14:03:29 +0300

    deleted all files for review

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { checkEmployees } from "./Employee";
-import { checkK9 } from "./K9";
-import { checkMotherOfDogs } from "./Mother_of_dogs";
-
-checkEmployees();
-checkK9();
-checkMotherOfDogs();
